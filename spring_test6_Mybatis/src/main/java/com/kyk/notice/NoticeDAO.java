fc645a6efc3a985cd75c49f2b0f796714991e501
Douner <Douner@Younkyung>
2016-05-11 15:14:55 +0900

    second

--- spring_test6_Mybatis/src/main/java/com/kyk/notice/NoticeDAO.java
+++ spring_test6_Mybatis/src/main/java/com/kyk/notice/NoticeDAO.java
@@ -5,13 +5,13 @@ import java.util.List;
 public interface NoticeDAO {
 	
 	//글목록조회
-	public List<NoticeDAO> list(int startNum, int lastNum)throws Exception;
+	public List<NoticeDTO> noticelist(int startNum, int lastNum)throws Exception;
 		
 	//글쓰기
-	public void write(NoticeDAO noticeDAO)throws Exception;
+	public void noticeWrite(NoticeDTO noticeDTO)throws Exception;
 	
 	//글보기
-	public NoticeDAO view(int num)throws Exception;
+	public NoticeDTO noticeView(int num)throws Exception;
 		
 	//총 페이지 수 반환
 	public int getTotalList()throws Exception;
